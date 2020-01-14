dc522d4e4d4ce65fc09a8fb2d9e3ca1c6040b4f0
Emily Giurleo <e.m.giurleo@gmail.com>
2020-01-13 22:37:09 -0500

    Updates

---
+++ precache-manifest.a27baf7d3e10cc50f868b7ab70b86778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dacdce3e38239300b1cccdf898d6fc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6c0622c80748a8e8527",
+    "url": "/static/css/main.0164c6b5.chunk.css"
+  },
+  {
+    "revision": "09a810af4c6509ceaa94",
+    "url": "/static/js/2.24721ff4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.24721ff4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6c0622c80748a8e8527",
+    "url": "/static/js/main.cea802c9.chunk.js"
+  },
+  {
+    "revision": "6e7d85ad969107dd34f7",
+    "url": "/static/js/runtime-main.54edee9e.js"
+  }
+]);
