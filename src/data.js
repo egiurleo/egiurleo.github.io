03c7f9123224a74a990ca7331354221ad2f3ebc7
Emily Giurleo <e.m.giurleo@gmail.com>
2020-03-25 17:26:59 -0400

    fixed broken link

--- src/data.js
+++ src/data.js
@@ -27,7 +27,7 @@ const data = {
   "projects": [
     {
       "title": "FreeFrom Compensation Compass",
-      "link": "https://compas.freefrom.org",
+      "link": "https://compass.freefrom.org",
       "year": 2019,
       "description": "<a href=\"https://freefrom.org\" target=\"_blank\" rel=\"noopener noreferrer\">FreeFrom</a> is an organization that helps survivors of gender-based violence achieve financial security. As a volunteer with <a href=\"https://ragtag.org\" target=\"_blank\" rel=\"noopener noreferrer\">Ragtag</a>, I built a Rails API backend for their new compensation tool, which helps survivors of gender-based violence in all 50 states learn what forms of financial compensation that are available to them. I also built a CMS with <a href=\"https://github.com/heartcombo/devise\" target=\"_blank\" rel=\"noopener noreferrer\">devise</a> and <a href=\"https://github.com/thoughtbot/administrate\" target=\"_blank\" rel=\"noopener noreferrer\">administrate</a> that allows the FreeFrom staff to update and maintain their site.",
       featured: true,
