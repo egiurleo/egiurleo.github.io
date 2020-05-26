c921ad6e10e99ab4b0565be81f8f6747ae34cc3d
Emily Giurleo <e.m.giurleo@gmail.com>
2020-05-25 23:45:09 -0400

    implemented first page of new design

---
+++ src/Home.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Email, Github, Twitter } from "./Social";
+import "./App.css";
+
+function Home() {
+  return (
+    <div className="Home">
+      <div className="container">
+        <div className="social-container">
+          <div className="social">
+            <Twitter />
+            <Github />
+            <Email />
+          </div>
+        </div>
+
+        <div className="text-container">
+          <div className="title-container">
+            <h1>Emily Giurleo</h1>
+            <div className="subtitle">Rubyist • Organizer • Cat lady</div>
+          </div>
+
+          <div className="about">
+            I am a software engineer who currently works on the{" "}
+            <a
+              href="https://github.com/mongodb/mongo-ruby-driver"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              MongoDB Ruby Driver
+            </a>
+            . In my spare time, you can find me organizing with{" "}
+            <a
+              href="https://persistbrooklyn.org"
+              target="blank"
+              rel="noopener noreferrer"
+            >
+              Persist Brooklyn
+            </a>{" "}
+            and looking at cats on the internet.
+          </div>
+        </div>
+
+        <div className="link-container">
+          <a href="/projects" className="projects">
+            Check out my projects &#8611;
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Social/index.js
@@ -0,0 +1,5 @@
+import Email from "./Email";
+import Github from "./Github";
+import Twitter from "./Twitter";
+
+export { Email, Github, Twitter };
