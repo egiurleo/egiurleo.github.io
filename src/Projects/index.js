eac3c6d676c2394fd12aad0b1d74886aab161041
Emily Giurleo <e.m.giurleo@gmail.com>
2020-05-28 00:12:53 -0400

    finishing touches on new version of site

--- src/Projects/index.js
+++ src/Projects/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Social from "../Social";
-import Project from './Project';
+import Project from "./Project";
 import data from "../data.json";
 import "./styles.css";
 
@@ -8,22 +8,28 @@ function Projects() {
   return (
     <div className="projects-component">
       <div className="header">
+        <a href="/">&#8610; home</a>
         <Social />
       </div>
 
-      <div className='content-container'>
+      <div className="content-container">
         <h1>Projects and Talks</h1>
 
-        <div className="projects-container">
-          {
-            data.map(project => 
-              <Project project={project} key={project.title}/>
-            )
-          }
-        </div>
+        {[2020, 2019, 2018, 2017].map((year) => {
+          return (
+            <div className="year-container" key={year}>
+              <h2>{year}</h2>
+              <div className="projects-container">
+                {data
+                  .filter((project) => project.year === year)
+                  .map((project) => (
+                    <Project project={project} key={project.title} />
+                  ))}
+              </div>
+            </div>
+          );
+        })}
       </div>
-
-      <div className="footer"><a href="/">&#8610; home</a></div>
     </div>
   );
 }
